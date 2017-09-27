b8eb8e704847ded4f11fcbd6629b803a7af4fe60
evgesha0411 <zhdanovichevgenia@gamil.com>
2017-09-27 16:30:18 +0300

    implementation of the task

--- src/fsm.js
+++ src/fsm.js
@@ -1,61 +1,214 @@
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
 
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
-    getState() {}
+    constructor(config) 
+	{
+		this.initialState = config.initial;
+		this.currentState = config.initial;
+		this.states = ['normal', 'busy', 'hungry', 'sleeping'];
+		this.events = ['study', 'get_hungry', 'get_tired', 'get_up', 'eat'];
+		this.counter = 0;
+		this.memory = [];
+		this.feature = [];
+	}
 
-    /**
-     * Goes to specified state.
-     * @param state
-     */
-    changeState(state) {}
+    getState() 
+	{
+		return this.currentState;
+	}
 
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-    trigger(event) {}
+    changeState(state) 
+	{
+		if(this.states.indexOf(state) != -1)
+		{
+			this.currentState = state;
+		}
+		else
+		{
+			throw new Error("Wrong state!!");
+		}
+	}
 
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
+    trigger(event) 
+	{
+		if(this.events.indexOf(event) != -1)
+		{
+			switch(event)
+			{
+				case this.events[0]: 
+				{
+					if(this.currentState == this.states[0]) 
+					{
+						if(this.memory.indexOf(this.currentState) == -1)
+						{
+							this.memory.push(this.currentState);
+						}
+								
+						this.currentState = this.states[1];
+											
+						if(this.feature.indexOf(this.currentState) == -1)
+						{
+							this.feature.push(this.currentState);
+						}
+						}
+						else
+						{
+							throw new Error("Wrong event!!");
+						}
+				} break;
+									 
+				case this.events[1]: 
+				{ 	
+					if(this.currentState == this.states[1] || this.currentState == this.states[3]) 
+					{
+						if(this.memory.indexOf(this.currentState) == -1)
+						{
+							this.memory.push(this.currentState);
+						}
+									
+						this.currentState = this.states[2];
+									
+						if(this.feature.indexOf(this.currentState) == -1)
+						{
+							this.feature.push(this.currentState);
+						}
+					}
+					else
+					{
+						throw new Error("Wrong event!!");
+					}
+				} break;
+										
+				case this.events[2]: 
+				{ 	
+					if(this.currentState == this.states[1]) 
+					{
+						if(this.memory.indexOf(this.currentState) == -1)
+						{
+							this.memory.push(this.currentState);
+						}
+											
+						this.currentState = this.states[3];
+						
+						if(this.feature.indexOf(this.currentState) == -1)
+						{
+							this.feature.push(this.currentState);
+						}
+					}
+					else
+					{
+						throw new Error("Wrong event!!");
+					}
+				} break;
+									 
+				case this.events[3]: 
+				{ 	
+					if(this.currentState == this.states[3]) 
+					{
+						if(this.memory.indexOf(this.currentState) == -1)
+						{
+							this.memory.push(this.currentState);
+						}
+											
+						this.currentState = this.states[0];
+									
+						if(this.feature.indexOf(this.currentState) == -1)
+						{
+							this.feature.push(this.currentState);
+						}
+					}
+					else
+					{
+						throw new Error("Wrong event!!");
+					}
+				} break;
+									 
+				case this.events[4]: 
+				{ 	
+					if(this.currentState == this.states[2]) 
+					{
+						if(this.memory.indexOf(this.currentState) == -1)
+						{
+							this.memory.push(this.currentState);
+						}
+											
+						this.currentState = this.states[0];
+											
+						if(this.feature.indexOf(this.currentState) == -1)
+						{
+							this.feature.push(this.currentState);
+						}
+					}
+					else
+					{
+						throw new Error("Wrong event!!");
+					}
+				} break;
+			}
+			this.counter++;
+		}
+		else
+		{
+			throw new Error("Wrong event!!");
+		}
+	}
 
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
+    reset() 
+	{
+		this.currentState = this.initialState;
+	}
 
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
+    getStates(event) 
+	{
+		if(event == undefined)
+		{
+			return this.states;
+		}
+		
+		var arr = [];
+		
+		switch(event)
+		{
+			case this.events[0]: { arr.push(this.states[0])} break;
+			case this.events[1]: { arr.push(this.states[1], this.states[3])} break;
+			case this.events[2]: { arr.push(this.states[1])} break;
+			case this.events[3]: { arr.push(this.states[2])} break;
+			case this.events[4]: { arr.push(this.states[3])} break;
+		}
+		
+		return arr;
+	}
 
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {}
+    undo() 
+	{
+		if(this.memory[this.counter-1] === undefined || this.counter <= 0)
+		{
+			this.currentState = this.states[0];
+			return false;
+		}
+		
+		this.currentState = this.memory[this.counter-1];
+		this.counter--;
+		return true;
+	}
 
-    /**
-     * Clears transition history
-     */
-    clearHistory() {}
+    redo() 
+	{
+		if(this.feature[this.counter] === undefined)
+		{
+			return false;
+		}
+		
+		this.currentState = this.feature[this.counter];
+		this.counter++;
+		return true;
+	}
+
+    clearHistory() 
+	{
+		this.memory = [];
+		this.feature = [];
+	}
 }
 
 module.exports = FSM;
 
-/** @Created by Uladzimir Halushka **/
